b5ea24c7a4e7e12e8f92084273372ee987376c63
milanzivanov <milan.zivanov@gmail.com>
2024-08-21 11:52:15 +0200

    initial commit vite + react + tailiwend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="text-3xl font-bold underline">Hello React!</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
